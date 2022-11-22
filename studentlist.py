e4e702734dccb4548e8bc70886033e6afc4468a8
layersony <layersony@gmail.com>
2022-11-22 13:02:22 +0300

    generate mystudents  list

--- studentlist.py
+++ studentlist.py
@@ -1,13 +1,19 @@
 import csv
+import sys
 
 def main():
+  filename = sys.argv[1]
   students = []
-  with open('student.csv') as csvfile:
+  with open(filename) as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
       wrd = row[0].split(',')
-      students.append(f'{wrd[1]} {wrd[0]}'.strip())
-  print(students)
+      if len(wrd) == 2:
+        stName = f'{wrd[1]} {wrd[0]}'.strip()
+        students.append(f'{stName}\n')
+        
+  with open('important_docs/myStudents.txt', 'w') as file1:
+    file1.writelines(students)
 
 if __name__ == "__main__":
   main()
